941cf396c856e004c1f34f002f0ad781775495b9
German Renato Cobian <germancobian@hotmail.com>
2023-03-14 23:20:13 -0700

    populate database from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+def populate_merchants
+  p 'Populating merchants'
+
+  File.open('./db/seeds/merchants.csv').each_line do |line|
+    attrs = line.strip.split(',')
+    Merchant.create(id: attrs[0], name: attrs[1], email: attrs[2], cif: attrs[3])
+  end
+
+  p 'Merchants populated successfully'
+end
+
+def populate_shoppers
+  p 'Populating shoppers'
+
+  File.foreach('./db/seeds/shoppers.csv') do |line|
+    attrs = line.strip.split(',')
+    Shopper.create(id: attrs[0], name: attrs[1], email: attrs[2], nif: attrs[3])
+  end
+
+  p 'Shoppers populated successfully'
+end
+
+def populate_orders
+  p 'Populating orders'
+
+  File.foreach('./db/seeds/orders.csv') do |line|
+    attrs = line.strip.split(',')
+
+    Order.create(
+      id: attrs[0],
+      merchant_id: attrs[1],
+      shopper_id: attrs[2],
+      amount: attrs[3],
+      created_at: attrs[4],
+      completed_at: attrs[5]
+    )
+  end
+
+  p 'Orders populated successfully'
+end
+
+populate_merchants
+populate_shoppers
+populate_orders
